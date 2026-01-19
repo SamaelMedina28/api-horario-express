d4b62326e099b940700159dd7a7f53321bc64f3f
Samael Home <samaelortiz2218@gmail.com>
2026-01-18 19:45:20 -0800

    fix: corregir error al mandar un array vacio

--- src/validations/subject.validation.ts
+++ src/validations/subject.validation.ts
@@ -1,8 +1,22 @@
 import { body } from "express-validator";
 
 export const subjectRules = [
-    body("subjects").notEmpty().withMessage("Tiene que enviar al menos una materia"),
-    body("subjects").isArray().withMessage("Debe enviar un array de materias"),
-    body("subjects.*.name").notEmpty().withMessage("El nombre de la materia es requerido"),
-    body("subjects.*.name").isLength({ min: 3 }).withMessage("El nombre de la materia debe tener al menos 3 caracteres"),
+  body("subjects")
+    .notEmpty()
+    .withMessage("Tiene que enviar al menos una materia"),
+  body("subjects")
+    .isArray()
+    .custom((value) => {
+      if (value.length === 0) {
+        throw new Error("Debe enviar al menos una materia");
+      }
+      return true;
+    })
+    .withMessage("Debe enviar al menos una materia"),
+  body("subjects.*.name")
+    .notEmpty()
+    .withMessage("El nombre de la materia es requerido"),
+  body("subjects.*.name")
+    .isLength({ min: 3 })
+    .withMessage("El nombre de la materia debe tener al menos 3 caracteres"),
 ];
