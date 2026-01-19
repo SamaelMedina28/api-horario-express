e84440ef877d53fcf25736c7566b6d110f3015e5
Samael Home <samaelortiz2218@gmail.com>
2026-01-18 21:54:08 -0800

    feat: añadir ruta de getSubjects

--- src/routes/subject.routes.ts
+++ src/routes/subject.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { createSubject } from "../controllers/subject.controller.js";
+import { createSubject, getSubjects } from "../controllers/subject.controller.js";
 import { subjectRules } from "../validations/subject.validation.js";
 import { validate } from "../middlewares/validate.js";
 
 const router = express.Router();
 
 router.post("/", subjectRules, validate, createSubject);
+router.get("/", getSubjects);
 
 export default router;
