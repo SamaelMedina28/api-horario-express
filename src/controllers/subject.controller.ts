4e24102a42fa88ee58fb1f787a5a21555e511352
Samael Home <samaelortiz2218@gmail.com>
2026-01-18 21:33:51 -0800

    fix: relacionar materia con el usuario propietario

--- src/controllers/subject.controller.ts
+++ src/controllers/subject.controller.ts
@@ -5,9 +5,10 @@ import Subject from "../models/Subject.js";
 export const createSubject = async (req: Request, res: Response) => {
     try {
         const { subjects } = req.body;
-        // const subject = await Subject.create(data);
         subjects.forEach(async (subject: string) => {
-            await Subject.create({ name: subject });
+            await Subject.create({ 
+              name: subject,
+              user: req.user?.id});
         });
 
         res.status(201).json({ message: "Materia creada exitosamente" });
