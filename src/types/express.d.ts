4b6b7e1cde9c6e3637770b3dbe7c9badef5add45
Samael Home <samaelortiz2218@gmail.com>
2026-01-18 21:53:10 -0800

    fix: arreglar problema en definicion de modelo de usuario

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,18 @@
+// types/express.d.ts
 import { JwtPayload } from "jsonwebtoken";
+
+interface User extends JwtPayload {
+  id: string;
+  email?: string;
+  isNew?: boolean;
+}
 
 declare global {
   namespace Express {
     interface Request {
-      user?: { id: string; isNew: boolean } | JwtPayload;
+      user?: UserPayload;
     }
   }
 }
+
+export {};
