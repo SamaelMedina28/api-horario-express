6b58e0540da2a83633bbe237edfdf631cc93a73e
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 13:12:44 -0800

    feat: crear metodo para eliminar subjects

--- src/controllers/subject.controller.ts
+++ src/controllers/subject.controller.ts
@@ -2,41 +2,55 @@ import type { Request, Response } from "express";
 import Subject from "../models/Subject.js";
 import { error } from "node:console";
 
-
 export const createSubject = async (req: Request, res: Response) => {
-    try {
-        const { subjects } = req.body;
-        subjects.forEach(async (subject: string) => {
-            await Subject.create({
-              name: subject,
-              user: req.user?.id});
-        });
+  try {
+    const { subjects } = req.body;
+    subjects.forEach(async (subject: string) => {
+      await Subject.create({
+        name: subject,
+        user: req.user?.id,
+      });
+    });
 
-        res.status(201).json({ message: "Materia creada exitosamente" });
-    } catch (error) {
-        res.status(500).json({ message: "Error al crear la materia" });
-    }
+    res.status(201).json({ message: "Materia creada exitosamente" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al crear la materia" });
+  }
 };
 
 export const getSubjects = async (req: Request, res: Response) => {
-    try {
-        const subjects = await Subject.find({ user: req.user?.id }).sort({
-            name: 1,
-        });
-        res.status(200).json(subjects);
-    } catch (error) {
-        res.status(500).json({ message: "Error al obtener las materias" });
-    }
+  try {
+    const subjects = await Subject.find({ user: req.user?.id }).sort({
+      name: 1,
+    });
+    res.status(200).json(subjects);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener las materias" });
+  }
 };
 
 export const editSubject = async (req: Request, res: Response) => {
-    try{
-        const { id } = req.params;
-        const { name } = req.body;
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
 
-        const subject = await Subject.findByIdAndUpdate(id, { name }, { new: true });
-        res.status(200).json({ message: "Materia editada exitosamente", subject });
-    }catch (err){
-        res.status(500).json({ message: "Error al editar la materia", error: err });
+    const subject = await Subject.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true },
+    );
+    res.status(200).json({ message: "Materia editada exitosamente", subject });
+  } catch (err) {
+    res.status(500).json({ message: "Error al editar la materia", error: err });
+  }
+};
+
+export const deleteSubject = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        await Subject.findByIdAndDelete(id);
+        return res.status(200).json({ message: "Materia eliminada exitosamente" });
+    } catch (error) {
+        return res.status(500).json({ message: "Error al eliminar la materia" });
     }
-}
+};
