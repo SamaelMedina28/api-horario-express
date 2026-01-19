dab8389b50a712c0220d7e4f116a45a4e1ac312a
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 12:44:31 -0800

    feat: crear metodo en controler para editar una materia

--- src/controllers/subject.controller.ts
+++ src/controllers/subject.controller.ts
@@ -1,12 +1,13 @@
 import type { Request, Response } from "express";
 import Subject from "../models/Subject.js";
+import { error } from "node:console";
 
 
 export const createSubject = async (req: Request, res: Response) => {
     try {
         const { subjects } = req.body;
         subjects.forEach(async (subject: string) => {
-            await Subject.create({ 
+            await Subject.create({
               name: subject,
               user: req.user?.id});
         });
@@ -27,3 +28,15 @@ export const getSubjects = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Error al obtener las materias" });
     }
 };
+
+export const editSubject = async (req: Request, res: Response) => {
+    try{
+        const { id } = req.params;
+        const { name } = req.body;
+
+        const subject = await Subject.findByIdAndUpdate(id, { name }, { new: true });
+        res.status(200).json({ message: "Materia editada exitosamente", subject });
+    }catch (err){
+        res.status(500).json({ message: "Error al editar la materia", error: err });
+    }
+}
