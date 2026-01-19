577527cd69489a0c0361c178f325357450a9b9aa
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 12:46:44 -0800

    feat: agregar ruta de edicion a subject.routes

--- src/routes/subject.routes.ts
+++ src/routes/subject.routes.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post("/", subjectRules, validate, createSubject);
 router.get("/", getSubjects);
+router.put("/:id", subjectRules, validate, createSubject);
 
 export default router;
