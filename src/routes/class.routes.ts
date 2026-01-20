425fe80bc0b79207aa4036ca0e455f63a0be376f
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 19:26:10 -0800

    feat: agregar ruta para metodo getClasses

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { createClassRules } from "../validations/class.validatons.js";
-import { createClass } from "../controllers/class.controller.js";
+import { createClass, getClasses } from "../controllers/class.controller.js";
 const router = express.Router();
 
 // Crear clases, editar clase, borrar clase, ver una clase, ver todas las clases
 router.post("/", createClassRules, validate, createClass);
-// router.get("/", getClasses);
+router.get("/", getClasses);
 // router.get("/:id", getClass);
 // router.put("/:id", editClassRules, validate, editClass);
 // router.delete("/:id", deleteClass);
