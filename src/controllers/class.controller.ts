9fbc0bda9900c82c27306bffbe65dde2d22c398b
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 19:20:12 -0800

    feat: crear metodo para crear clases

---
+++ src/controllers/class.controller.ts
@@ -0,0 +1,28 @@
+import { response, type Request, type Response } from "express";
+import type { Class as TypeClass } from "../types/express.js";
+import Class from "../models/Class.js";
+
+export const createClass = (req: Request, res: Response) => {
+  // Nos vendran estos datos (day, subject, startTime, endTime, teacher, classType, classroom, building) pero en una variable que sera array de objetos
+  try {
+    const { classes } = req.body;
+    const userId = req.user?.id;
+    classes.forEach(async (classItem: TypeClass) => {
+      // Aqui se deberia guardar en la base de datos
+      await Class.create({
+        day: classItem.day,
+        subject: classItem.subject,
+        startTime: classItem.startTime,
+        endTime: classItem.endTime,
+        teacher: classItem.teacher,
+        classType: classItem.classType,
+        classroom: classItem.classroom,
+        building: classItem.building,
+        user: userId,
+      });
+      res.status(201).json({ message: "Clase creada exitosamente" });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error al crear la clase", error });
+  }
+};
