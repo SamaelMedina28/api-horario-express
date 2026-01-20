8ba5d0aa85ac77c996784bfe807e604f74cc5db5
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 19:31:48 -0800

    feat: añadir endpoint para el metodo getClassById

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { createClassRules } from "../validations/class.validatons.js";
-import { createClass, getClasses } from "../controllers/class.controller.js";
+import { createClass, getClassById, getClasses } from "../controllers/class.controller.js";
 const router = express.Router();
 
 // Crear clases, editar clase, borrar clase, ver una clase, ver todas las clases
 router.post("/", createClassRules, validate, createClass);
 router.get("/", getClasses);
-// router.get("/:id", getClass);
+router.get("/:id", getClassById);
 // router.put("/:id", editClassRules, validate, editClass);
 // router.delete("/:id", deleteClass);
 
