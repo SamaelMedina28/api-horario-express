bd1488d03e8ec664ef10b78717328611466611b2
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 19:21:35 -0800

    feat: agregar ruta metodo y middlewares

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
+import { createClassRules } from "../validations/class.validatons.js";
+import { createClass } from "../controllers/class.controller.js";
 const router = express.Router();
 
 // Crear clases, editar clase, borrar clase, ver una clase, ver todas las clases
 router.post("/", createClassRules, validate, createClass);
-router.get("/", getClasses);
-router.get("/:id", getClass);
-router.put("/:id", editClassRules, validate, editClass);
-router.delete("/:id", deleteClass);
+// router.get("/", getClasses);
+// router.get("/:id", getClass);
+// router.put("/:id", editClassRules, validate, editClass);
+// router.delete("/:id", deleteClass);
 
 export default router;
