5b9b279321a8c55ca249070b842ba9130f248fe8
Samael Home <samaelortiz2218@gmail.com>
2026-01-19 19:32:21 -0800

    feat: añadir endpoint para metodo deleteClass

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { createClassRules } from "../validations/class.validatons.js";
-import { createClass, getClassById, getClasses } from "../controllers/class.controller.js";
+import { createClass, getClassById, getClasses, deleteClass } from "../controllers/class.controller.js";
 const router = express.Router();
 
 // Crear clases, editar clase, borrar clase, ver una clase, ver todas las clases
@@ -9,6 +9,6 @@ router.post("/", createClassRules, validate, createClass);
 router.get("/", getClasses);
 router.get("/:id", getClassById);
 // router.put("/:id", editClassRules, validate, editClass);
-// router.delete("/:id", deleteClass);
+router.delete("/:id", deleteClass);
 
 export default router;
